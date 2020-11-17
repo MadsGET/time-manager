1e3021255093a971c9c072bd185134cfd2fcf6ad
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-11-17 11:48:02 +0100

    Archive Restructure

--- js/archive.js
+++ js/archive.js
@@ -1,8 +1,5 @@
 // Variables
-let archiveData = [];
-let archiveStatistics = [];
-let archivePages = [];
-let archivePagesIndex = 0;
+let diagramData = [];
 
 class DiagramData
 {
@@ -10,6 +7,10 @@ class DiagramData
 	{
 		this.key = time.months[date.getMonth()].name;
 		this.items = [];
+		this.totalTime = 0;
+		this.averageTime = 0;
+		this.highestTime = 0;
+
 
 		// Loop through value array and setup to month size.
 		for (let i = 0; i < date.getDate(); i++)
@@ -26,13 +27,27 @@ class DiagramData
 			// Guard clause; If empty.
 			if (data == '') return;
 
+			// Data unpacking references.
 			const day = parseInt(data.slice(0, 2));
 			const hours = parseInt(data.slice(3, 5));
 			const minutes = parseInt(data.slice(5, 7));
 
+			// Calculate the total value of hours and minutes.
+			const dataValue = hours + parseFloat((minutes / 60).toFixed(1));
+
 			// Set data within items.
-			this.items[day] = hours + parseFloat((minutes / 60).toFixed(1));
+			this.items[day] = dataValue;
+
+			// Add value to total and average time.
+			this.totalTime += dataValue;
+			this.averageTime += dataValue;
+
+			// Set highest time value.
+			if (this.highestTime < dataValue) this.highestTime = dataValue;
 		}
+
+		// Calculate average time spent.
+		this.averageTime = (this.averageTime / date.getDate());
 	}
 }
 
@@ -48,45 +63,12 @@ function initalizeArchive() // Live update???
 			let datasetDate = new Date(todaysDate.getFullYear(), (i + 1), 0);
 
 			// Setup archive data.
-			archiveData[i] = new DiagramData(datasetDate);
-
-			// Setup arhive pages.
-			archivePages.push({name: archiveData[i].key, reference: i});
-
-			// Setup statistics variables.
-			let totalTime = 0, averageTime = 0, leadingValue = 0;
-
-			for (let x = 0; x < archiveData[i].items.length; x++)
-			{
-				// Setup item value as a floating point number with only one decimal.
-				let hours = archiveData[i].items[x].hours;
-				let minutes = archiveData[i].items[x].minutes;
-				const itemValue = hours + parseFloat((minutes / 60).toFixed(1));
-
-				// Add to total time.
-				totalTime += itemValue;
-
-				// Add to average.
-				averageTime += itemValue;
-
-				// Record highest value
-				if (leadingValue < itemValue) leadingValue = itemValue;
-			}
-
-			// Set archive pages index.
-			archivePagesIndex = (archivePages.length -1);
-
-			// Calculate average time.
-			averageTime = averageTime / archiveData[i].items.length;
-
-			// Setup archive statistics.
-			archiveStatistics[i] = { totalTime: totalTime, averageTime: averageTime, highestValue: Math.round(leadingValue), length: datasetDate.getDate() };
+			diagramData[i] = new DiagramData(datasetDate);
 		}
 		else
 		{
 			// Event of there being no data??? Execution to allow a day to be generated before this intilizations??
-			archiveData[i] = null;
-			archiveStatistics[i] = null;
+			diagramData[i] = null;
 		}
 	}
 } 
